fbb712d20761ef565c02259c961a8c9cefc56a0c
Christopher Oliver <coliver650@gmail.com>
2015-05-29 17:19:33 -0400

    Built an abstract class for the queue and stack to use as most of the functionality related to these two data structures are the same.

---
+++ ListBasedStructure.java
@@ -0,0 +1,45 @@
+package com.oliver;
+
+abstract class ListBasedStructure {
+
+    protected LinkedList list;
+
+    public ListBasedStructure() {
+
+        list = new LinkedList();
+    }
+
+    abstract void inject(Object data);
+
+    //  -> Object
+    // EFFECT: Remove and return the first object in the list.
+    // Strategy: Function Composition
+    public Object eject() {
+
+        if(list.getSize() == 0) {
+            return null;
+        } else {
+            Object data = list.getEntry(1).getValue();
+            list.remove(1);
+            return data;
+        }
+    }
+
+    //  -> int
+    // Return the number of objects in the list.
+    // Strategy: Function Composition
+    public int getSize() {
+
+        return list.getSize();
+    }
+
+    //  -> void
+    // EFFECT: Clear the list of all objects.
+    // Strategy: Function Composition
+    public void clear() {
+
+        list.clear();
+    }
+
+    abstract void print();
+}
--- Queue.java
+++ Queue.java
@@ -1,12 +1,10 @@
 package com.oliver;
 
-public class Queue {
-
-    private LinkedList list;
+public class Queue extends ListBasedStructure {
 
     public Queue() {
 
-        list = new LinkedList();
+        super();
     }
 
     // Object -> void
@@ -16,36 +14,6 @@ public class Queue {
 
         LinkedListEntry entry = new LinkedListEntry(data);
         list.addTail(entry);
-    }
-
-    //  -> Object
-    // EFFECT: Remove and return the first object in the list.
-    // Strategy: Function Composition
-    public Object eject() {
-
-        if(list.getSize() == 0) {
-            return null;
-        } else {
-            Object data = list.getEntry(1).getValue();
-            list.remove(1);
-            return data;
-        }
-    }
-
-    //  -> int
-    // Return the number of objects in the list.
-    // Strategy: Function Composition
-    public int getSize() {
-
-        return list.getSize();
-    }
-
-    //  -> void
-    // EFFECT: Clear the list of all objects.
-    // Strategy: Function Composition
-    public void clear() {
-
-        list.clear();
     }
 
     //  -> void
